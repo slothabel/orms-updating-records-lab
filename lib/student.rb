44df5debb7a3354eb1f91df3882c56a20ac90e37
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-16 19:18:06 -0400

    commit tree ca59d3b8826bbe2632c6ea629648f5c36714f6d5

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,73 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
+  attr_accessor :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+  sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+        DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end 
 
 
-end
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.new_from_db(row)
+  #  binding.pry 
+   student_id = row[0]
+   student_name = row[1]
+   student_grade = row[2]
+   self.new(student_name, student_grade, student_id)
+
+  end 
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
+    # binding.pry
+  end
+
+
+
+
+
+
+
+
+
+end#class ender
